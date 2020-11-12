fa2ad8ba1fef30f1f68221ff0371e0e21eb85d80
Cynthia <cbutarbu@ucsd.edu>
2020-11-11 23:27:53 -0800

    Sphere w/ GL_QUADS

---
+++ Sphere.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <fstream>
+#include <map>
+#include <tuple>
+#include <numeric>
+
+#include "Sphere.h"
+
+/* Code sampled from https://gist.github.com/zwzmzd/0195733fa1210346b00d, adjusted to use GL_QUADS instead of GL_QUADS_STRIP */
+Sphere::Sphere() {
+    int i, j;
+    std::vector<GLfloat> vertices;
+    std::vector<GLuint> indices;
+    std::vector<GLfloat> normals;
+    int indicator = 0;
+    for (i = 0; i <= stackCount; i++) {
+        double lat0 = glm::pi<double>() * (-0.5 + (double)(i - 1) / stackCount);
+        double z0 = sin(lat0);
+        double zr0 = cos(lat0);
+
+        double lat1 = glm::pi<double>() * (-0.5 + (double)i / stackCount);
+        double z1 = sin(lat1);
+        double zr1 = cos(lat1);
+        for (j = 0; j <= sectorCount; j++) {
+            double lng = 2 * glm::pi<double>() * (double)(j - 1) / sectorCount;
+            double lng1 = 2 * glm::pi<double>() * (double)(j) / sectorCount;
+
+            double x1 = cos(lng1);
+            double y1 = sin(lng1);
+            double x = cos(lng);
+            double y = sin(lng);
+
+            vertices.push_back(x * zr1);
+            vertices.push_back(y * zr1);
+            vertices.push_back(z1);
+            indices.push_back(indicator);
+            indicator++;
+
+            vertices.push_back(x * zr0);
+            vertices.push_back(y * zr0);
+            vertices.push_back(z0);
+            indices.push_back(indicator);
+            indicator++;
+
+            vertices.push_back(x1 * zr0);
+            vertices.push_back(y1 * zr0);
+            vertices.push_back(z0);
+            indices.push_back(indicator);
+            indicator++;
+
+            vertices.push_back(x1 * zr1);
+            vertices.push_back(y1 * zr1);
+            vertices.push_back(z1);
+            indices.push_back(indicator);
+            indicator++;
+        }
+        indices.push_back(GL_PRIMITIVE_RESTART_FIXED_INDEX);
+    }
+
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(0);
+
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    numsToDraw = indices.size();
+}
+
+void Sphere::
+draw(const glm::mat4& world, const glm::mat4& projection, const glm::mat4& view, const glm::vec3& eye) {
+    glUseProgram(shader);
+
+    glUniformMatrix4fv(glGetUniformLocation(shader, "view"), 1, false, glm::value_ptr(view));
+    glUniformMatrix4fv(glGetUniformLocation(shader, "projection"), 1, false, glm::value_ptr(projection));
+    glUniformMatrix4fv(glGetUniformLocation(shader, "model"), 1, GL_FALSE, glm::value_ptr(world));
+
+    glBindVertexArray(vao);
+    
+    glPatchParameteri(GL_PATCH_VERTICES, 4);
+    glEnable(GL_PRIMITIVE_RESTART);
+    glPrimitiveRestartIndex(GL_PRIMITIVE_RESTART_FIXED_INDEX);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glDrawElements(GL_QUADS, numsToDraw, GL_UNSIGNED_INT, NULL);
+    
+
+    //glDrawElements(GL_TRIANGLES, numsToDraw, GL_UNSIGNED_INT, 0);
+    glUseProgram(0);
+    glBindVertexArray(0);
+}
---
+++ Sphere.h
@@ -0,0 +1,26 @@
+#ifndef _SPHERE_H_
+#define _SPHERE_H_
+
+#ifdef __APPLE__
+#include <OpenGL/gl3.h>
+#else
+#include <GL/glew.h>
+#endif
+
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Sphere {
+	public:
+		GLuint shader;
+		GLuint vao = 0, vbo = 0, ebo = 0;
+
+		int stackCount = 40;
+		int sectorCount = 40;
+		int numsToDraw;
+		Sphere();
+		void draw(const glm::mat4& world, const glm::mat4& projection, const glm::mat4& view, const glm::vec3& eye) override;
+};
+
+#endif
